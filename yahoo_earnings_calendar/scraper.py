fba0504554e3f0c72f24dfc14c42428e9f54d294
lewis <lewistham9x@gmail.com>
2021-11-02 05:32:20 +0800

    Changed header

--- yahoo_earnings_calendar/scraper.py
+++ yahoo_earnings_calendar/scraper.py
@@ -1,14 +1,14 @@
-'''
+"""
 Yahoo! Earnings Calendar scraper
-'''
+"""
 import datetime
 import json
 import logging
 import requests
 import time
 
-BASE_URL = 'https://finance.yahoo.com/calendar/earnings'
-BASE_STOCK_URL = 'https://finance.yahoo.com/quote'
+BASE_URL = "https://finance.yahoo.com/calendar/earnings"
+BASE_STOCK_URL = "https://finance.yahoo.com/quote"
 RATE_LIMIT = 2000.0
 SLEEP_BETWEEN_REQUESTS_S = 60 * 60 / RATE_LIMIT
 OFFSET_STEP = 100
@@ -16,8 +16,7 @@ OFFSET_STEP = 100
 # Logging config
 logger = logging.getLogger()
 handler = logging.StreamHandler()
-formatter = logging.Formatter(
-    '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+formatter = logging.Formatter("%(asctime)s %(name)-12s %(levelname)-8s %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.ERROR)
@@ -28,18 +27,25 @@ class YahooEarningsCalendar(object):
     This is the class for fetching earnings data from Yahoo! Finance
     """
 
-    def __init__(self, delay=SLEEP_BETWEEN_REQUESTS_S, timeout=(2,5)):
+    def __init__(self, delay=SLEEP_BETWEEN_REQUESTS_S, timeout=(2, 5)):
         self.delay = delay
         self.timeout = timeout
 
     def _get_data_dict(self, url, proxies):
+        headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+        }
         time.sleep(self.delay)
-        USER_AGENT = { 'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36') }
-        page = requests.get(url, proxies=proxies,timeout=self.timeout, headers=USER_AGENT)
-        page_content = page.content.decode(encoding='utf-8', errors='strict')
-        page_data_string = [row for row in page_content.split(
-            '\n') if row.startswith('root.App.main = ')][0][:-1]
-        page_data_string = page_data_string.split('root.App.main = ', 1)[1]
+        print("Getting calendar")
+
+        page = requests.get(url, headers=headers)
+        page_content = page.content.decode(encoding="utf-8", errors="strict")
+        page_data_string = [
+            row
+            for row in page_content.split("\n")
+            if row.startswith("root.App.main = ")
+        ][0][:-1]
+
         return json.loads(page_data_string)
 
     def get_next_earnings_date(self, symbol, proxies={}):
@@ -51,12 +57,14 @@ class YahooEarningsCalendar(object):
         Raises:
             Exception: When symbol is invalid or earnings date is not available
         """
-        url = '{0}/{1}'.format(BASE_STOCK_URL, symbol)
+        url = "{0}/{1}".format(BASE_STOCK_URL, symbol)
         try:
-            page_data_dict = self._get_data_dict(url,proxies=proxies)
-            return page_data_dict['context']['dispatcher']['stores']['QuoteSummaryStore']['calendarEvents']['earnings']['earningsDate'][0]['raw']
+            page_data_dict = self._get_data_dict(url, proxies=proxies)
+            return page_data_dict["context"]["dispatcher"]["stores"][
+                "QuoteSummaryStore"
+            ]["calendarEvents"]["earnings"]["earningsDate"][0]["raw"]
         except:
-            raise Exception('Invalid Symbol or Unavailable Earnings Date')
+            raise Exception("Invalid Symbol or Unavailable Earnings Date")
 
     def earnings_on(self, date, offset=0, count=1, proxies={}):
         """Gets earnings calendar data from Yahoo! on a specific date.
@@ -86,20 +94,20 @@ class YahooEarningsCalendar(object):
             return []
 
         if not isinstance(date, datetime.date):
-            raise TypeError(
-                'Date should be a datetime.date object')
-        date_str = date.strftime('%Y-%m-%d')
-        logger.debug('Fetching earnings data for %s', date_str)
-        dated_url = '{0}?day={1}&offset={2}&size={3}'.format(
-            BASE_URL, date_str, offset, OFFSET_STEP)
-        page_data_dict = self._get_data_dict(dated_url,proxies=proxies)
-        stores_dict = page_data_dict['context']['dispatcher']['stores']
-        earnings_count = stores_dict['ScreenerCriteriaStore']['meta']['total']
+            raise TypeError("Date should be a datetime.date object")
+        date_str = date.strftime("%Y-%m-%d")
+        logger.debug("Fetching earnings data for %s", date_str)
+        dated_url = "{0}?day={1}&offset={2}&size={3}".format(
+            BASE_URL, date_str, offset, OFFSET_STEP
+        )
+        page_data_dict = self._get_data_dict(dated_url, proxies=proxies)
+        stores_dict = page_data_dict["context"]["dispatcher"]["stores"]
+        earnings_count = stores_dict["ScreenerCriteriaStore"]["meta"]["total"]
 
         # Recursively fetch more earnings on this date
         new_offset = offset + OFFSET_STEP
         more_earnings = self.earnings_on(date, new_offset, earnings_count, proxies={})
-        curr_offset_earnings = stores_dict['ScreenerResultsStore']['results']['rows']
+        curr_offset_earnings = stores_dict["ScreenerResultsStore"]["results"]["rows"]
 
         return curr_offset_earnings + more_earnings
 
@@ -128,12 +136,11 @@ class YahooEarningsCalendar(object):
             TypeError: When either from_date or to_date is not a datetime.date object.
         """
         if from_date > to_date:
-            raise ValueError(
-                'From-date should not be after to-date')
-        if not (isinstance(from_date, datetime.date) and
-                isinstance(to_date, datetime.date)):
-            raise TypeError(
-                'From-date and to-date should be datetime.date objects')
+            raise ValueError("From-date should not be after to-date")
+        if not (
+            isinstance(from_date, datetime.date) and isinstance(to_date, datetime.date)
+        ):
+            raise TypeError("From-date and to-date should be datetime.date objects")
         earnings_data = []
         current_date = from_date
         delta = datetime.timedelta(days=1)
@@ -151,22 +158,23 @@ class YahooEarningsCalendar(object):
         Raises:
             Exception: When symbol is invalid or earnings date is not available
         """
-        url = 'https://finance.yahoo.com/calendar/earnings?symbol={0}'.format(symbol)
-        try: 
-            page_data_dict = self._get_data_dict(url,proxies=proxies)
-            return page_data_dict["context"]["dispatcher"]["stores"]["ScreenerResultsStore"]["results"]["rows"]
-        except: 
-            raise Exception('Invalid Symbol or Unavailable Earnings Date')
+        url = "https://finance.yahoo.com/calendar/earnings?symbol={0}".format(symbol)
+        try:
+            page_data_dict = self._get_data_dict(url, proxies=proxies)
+            return page_data_dict["context"]["dispatcher"]["stores"][
+                "ScreenerResultsStore"
+            ]["results"]["rows"]
+        except:
+            raise Exception("Invalid Symbol or Unavailable Earnings Date")
 
-if __name__ == '__main__':  # pragma: no cover
-    date_from = datetime.datetime.strptime(
-        'Feb 1 2018  10:00AM', '%b %d %Y %I:%M%p')
-    date_to = datetime.datetime.strptime(
-        'Feb 4 2018  1:00PM', '%b %d %Y %I:%M%p')
+
+if __name__ == "__main__":  # pragma: no cover
+    date_from = datetime.datetime.strptime("Feb 1 2018  10:00AM", "%b %d %Y %I:%M%p")
+    date_to = datetime.datetime.strptime("Feb 4 2018  1:00PM", "%b %d %Y %I:%M%p")
     yec = YahooEarningsCalendar()
     print(yec.earnings_on(date_from))
     print(yec.earnings_between(date_from, date_to))
     # Returns the next earnings date of BOX in Unix timestamp
-    print(yec.get_next_earnings_date('box'))
+    print(yec.get_next_earnings_date("box"))
     # Returns a list of all available earnings of BOX
-    print(yec.get_earnings_of('box'))
+    print(yec.get_earnings_of("box"))
